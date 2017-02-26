d8c57be452d7ad1eb6c79c97fb24847c708913d7
Theodore Liu <theodoretliu@gmail.com>
2017-02-26 10:27:15 -0500

    Initial commit

---
+++ start.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+from __future__ import print_function
+
+import sys
+import socket
+import json
+
+def connect():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("test-exch-teamname", 25000))
+    return s.makefile('rw', 1)
+
+def write(exchange, obj):
+    json.dump(obj, exchange)
+    exchange.write("\n")
+
+def read(exchange):
+    return json.loads(exchange.readline())
+
+def main():
+    exchange = connect()
+    write(exchange, {"type": "hello", "team": "TEAMNAME"})
+    hello_from_exchange = read(exchange)
+    print("The exchange replied:", hello_from_exchange, file=sys.stderr)
+
+if __name__ == "__main__":
+    main()
